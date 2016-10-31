295edcc6b1be0efd6b5f9b338e4f547f9e38d8eb
root <root@casrv01.webca.com.br>
2016-10-31 03:51:43 +0000

    up files

--- public/system/modification/catalog/language/portuguese-br/common/footer.php
+++ public/system/modification/catalog/language/portuguese-br/common/footer.php
@@ -14,7 +14,7 @@ $_['text_account']      = 'Minha conta';
 $_['text_order']        = 'Histórico de pedidos';
 $_['text_wishlist']     = 'Lista de desejos';
 $_['text_newsletter']   = 'Informativo';
-$_['text_powered']      = 'Desenvolvido por <a href="http://lojavirtual.digital" target="_blank">LojaVirtual.digital</a> - %s &copy; %s - Todos os direitos reservados';
+$_['text_powered']      = '%s &copy; %s - Todos os direitos reservados'; //Desenvolvido por <a href="http://lojavirtual.digital" target="_blank">LojaVirtual.digital</a> - 
 
 				$_['text_simple_blog']   = 'Blog';
 			
